41263de711f53483de343de7bf778f58cd9c49d6
Ozmundooo <osman.ali.dev@gmail.com>
2023-02-09 14:30:19 -0500

    Programs page created

---
+++ components/ProgramCard.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { urlFor } from "../lib/sanity/client";
+import { PortableText } from "@portabletext/react";
+import Link from "next/link";
+
+export default function ProgramCard({ program }) {
+  const components = {
+    block: {
+      // Ex. 1: customizing common block types
+      h1: ({ children }) => (
+        <h1 className="text-md line-clamp-1 font-['Open Sans']">{children}</h1>
+      ),
+      normal: ({ children }) => (
+        <p className="text-md line-clamp-1 font-['Open Sans']">{children}</p>
+      ),
+    },
+  };
+
+  return (
+    <Link
+      className=" cursor"
+      href={`work/${program.slug.current}`}
+      key={program.id}
+    >
+      <div className="w-[320px] lg:w-[400px]">
+        <h3 className="text-xl font-bold lg:text-2xl font-['Philosopher'] py-2">
+          {program.title}
+        </h3>
+        <img
+          className="h-[250px]  object-cover mb-2"
+          src={urlFor(program.mainImage)}
+        ></img>
+        <PortableText
+          className="line-clamp-3"
+          value={program.content}
+          components={components}
+        />
+        <p className="font-['Open Sans'] font-semibold mt-3 underline">
+          See more
+        </p>
+      </div>
+    </Link>
+  );
+}
---
+++ pages/programs.js
@@ -0,0 +1,46 @@
+import React from "react";
+import ProgramCard from "../components/ProgramCard";
+import { sanityClient } from "../lib/sanity/client";
+import { programsQuery } from "../lib/sanity/programsQuery";
+import programHero from "../assets/HomeHeroBg.png";
+import Image from "next/image";
+export default function programs({ programs }) {
+  return (
+    <div>
+      <div className="">
+        <div class="relative ">
+          <Image
+            src={programHero}
+            className="opacity-[40%] object-cover  xl:h-[30vh] w-full"
+          />
+          <h1 class="absolute top-1/2 ml-8 lg:ml-16 xl:ml-20  -translate-y-1/2 text-3xl lg:text-4xl font-['Philosopher'] text-white font-semibold">
+            Programs
+          </h1>
+        </div>
+      </div>
+
+      <div className="grid xl:grid-cols-3 md:grid-cols-2 gap-3 place-items-center my-10">
+        {programs.map((program) => (
+          <ProgramCard program={program} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const programs = await sanityClient.fetch(programsQuery);
+
+  if (!programs.length) {
+    return {
+      props: {
+        programs: [],
+      },
+    };
+  } else
+    return {
+      props: {
+        programs,
+      },
+    };
+}
