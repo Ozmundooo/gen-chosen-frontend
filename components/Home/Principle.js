f17c9f0eb340550c3051a4fb3b17488f3a328df9
Ozmundooo <osman.ali.dev@gmail.com>
2023-02-13 22:33:31 -0500

    changes home page

--- components/Home/Principle.js
+++ components/Home/Principle.js
@@ -3,10 +3,10 @@ import PrincipleCard from "./PrincipleCard";
 export default function Principles({ principles }) {
   return (
     <>
-      <h3 className="text-3xl text-center font-bold lg:text-4xl font-['Raleway'] py-2 mt-20">
+      <h3 className="text-3xl text-center font-bold lg:text-4xl font-['Raleway'] mb-8 py-2 mt-20">
         Principles
       </h3>
-      <p className="text-sm lg:text-xl text-center font-['Poppins'] font-normal xl:mx-80">
+      <p className="text-sm lg:text-xl text-center font-['Poppins'] font-normal mb-10 xl:mx-80">
         Lorem Ipsum is simply dummy text of the printing and typesetting
         industry. Lorem Ipsum has been the industry's standard dummy text ever
         since the 1500s, when an unknown printer took a galley of type and
--- components/Home/PrincipleCard.js
+++ components/Home/PrincipleCard.js
@@ -3,11 +3,11 @@ import { urlFor } from "../../lib/sanity/client";
 export default function PrincipleCards({ principle }) {
   return (
     <div className="lg:w-1/3 text-center my-5">
-      <img className="w-1/2 mx-auto" src={urlFor(principle.image)} />
+      <img className="w-1/4 mx-auto" src={urlFor(principle.image)} />
       <h3 className="text-2xl font-semibold lg:text-4xl font-['Raleway'] py-2">
         {principle.title}
       </h3>
-      <p className="text-md lg:text-lg font-['Poppins'] px-20 py-1">
+      <p className="text-md lg:text-lg font-['Poppins'] px-24 py-1">
         {principle.description}
       </p>
     </div>
