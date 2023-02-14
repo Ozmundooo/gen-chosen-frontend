c8058d999efd498658484bbfefad070d58c0d027
Ozmundooo <osman.ali.dev@gmail.com>
2023-02-13 22:30:18 -0500

    changed _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 import "../styles/globals.css";
-import React from "react";
-export default function MyApp({ Component, pageProps }) {
+
+function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
   );
 }
+
+export default MyApp;
